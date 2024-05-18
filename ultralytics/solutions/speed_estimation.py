9a71515511c5bc63716bb601660233137406074d
Danish Barkat <83992824+Dan-445@users.noreply.github.com>
2024-05-18 02:38:12 -0700

    speed_estimation.py

--- ultralytics/solutions/speed_estimation.py
+++ ultralytics/solutions/speed_estimation.py
@@ -148,8 +148,9 @@ class SpeedEstimator:
             time_difference = time() - self.trk_previous_times[trk_id]
             if time_difference > 0:
                 dist_difference = np.abs(track[-1][1] - self.trk_previous_points[trk_id][1])
-                speed = (dist_difference / time_difference) * 0.621371
-                self.dist_data[trk_id] = speed
+                speed = dist_difference / time_difference
+                speed_mph= speed  * 0.621371
+                self.dist_data[trk_id] = speed_mph
 
         self.trk_previous_times[trk_id] = time()
         self.trk_previous_points[trk_id] = track[-1]
